c57b345a7a16fbb439b2f103d2397d1b91fdef08
Aydin Ahmadli <35424859+buzzer4mornin@users.noreply.github.com>
2020-09-28 18:48:03 +0200

    fill in code

--- text_to_csv_CZ.py
+++ text_to_csv_CZ.py
@@ -1,1 +1,10 @@
+import csv
+import codecs
+
+with codecs.open('TEXTCZ1.txt', 'r', encoding='ISO-8859-2',  errors='replace') as in_file:
+    stripped = (line.strip() for line in in_file)
+    lines = (line.split(",") for line in stripped if line)
+    with open('myfile_CZ.csv', 'w') as out_file:
+        writer = csv.writer(out_file)
+        writer.writerows(lines)
 
